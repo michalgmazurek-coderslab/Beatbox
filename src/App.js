ab159114113a437302e7b4d9fcd9f334fdaea2cb
michalgmazurek-coderslab <michalgmazurek@gmail.com>
2017-12-28 15:12:35 +0100

    In progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class App extends Component {
   render() {
-    return (
-        <div className="beatbox__container">
-
-        </div>
-    );
+    return (<div className="beatbox">
+      <div className="beatbox__container">
+        <div className="beatbox__sound-btn" id="btn-1"></div>
+        <div className="beatbox__sound-btn" id="btn-2"></div>
+        <div className="beatbox__sound-btn" id="btn-3"></div>
+        <div className="beatbox__sound-btn" id="btn-4"></div>
+        <div className="beatbox__sound-btn" id="btn-5"></div>
+        <div className="beatbox__sound-btn" id="btn-6"></div>
+        <div className="beatbox__sound-btn" id="btn-7"></div>
+        <div className="beatbox__sound-btn" id="btn-8"></div>
+        <div className="beatbox__sound-btn" id="btn-9"></div>
+        <div className="beatbox__sound-btn" id="btn-10"></div>
+        <div className="beatbox__sound-btn" id="btn-11"></div>
+        <div className="beatbox__sound-btn" id="btn-12"></div>
+        <div className="beatbox__sound-btn" id="btn-13"></div>
+        <div className="beatbox__sound-btn" id="btn-14"></div>
+        <div className="beatbox__sound-btn" id="btn-15"></div>
+        <div className="beatbox__sound-btn" id="btn-16"></div>
+        <div className="beatbox__sound-btn" id="btn-17"></div>
+        <div className="beatbox__sound-btn" id="btn-18"></div>
+        <div className="beatbox__sound-btn" id="btn-19"></div>
+        <div className="beatbox__sound-btn" id="btn-20"></div>
+      </div>
+    </div>);
   }
 }
 
