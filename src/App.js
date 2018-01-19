98079b7d348101e6bc6cee8cddbda6aa344c2951
michalgmazurek-coderslab <michalgmazurek@gmail.com>
2018-01-19 08:24:20 +0100

    sounds

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ class App extends Component {
 
   playSounds = (event) => {
     let audio = document.createElement("audio");
-    audio.src = "sounds/" + event.target.id + ".mp3";
+    audio.src = "./sounds/" + event.target.id + ".mp3";
     audio.currentTime = 0;
     audio.play();
   }
   render() {
 let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     let sounds = arr.map((i) => {
-      let src = `/sounds/${i}.mp3`;
+      let src = `./sounds/${i}.mp3`;
       let toReturn = <audio key={i} src={src} preload="true"></audio>;
       return toReturn;
     });
